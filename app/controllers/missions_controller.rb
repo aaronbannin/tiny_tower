9f0cf2d23c8457d8ab9b1ac11abbd9fcae98f80a
aaronbannin <aaronbannin@gmail.com>
2014-07-14 10:15:54 -0700

    reorder mission views, db issues

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -13,6 +13,6 @@ class MissionsController < ApplicationController
         @show_missions = Mission.all.order("id")
         @open_missions = Mission.all.where("is_complete is false")
         @missions_audit = Mission.group("is_complete").count
-        @missions_eligible = Mission.all.where("is_eligible is true and is_complete is false")
+        @missions_eligible = Mission.all.where("is_eligible is true and is_complete is false").order("id")
     end
 end
